6d59ad8f17314a4fbe930479becfe85b9be354fb
Keith Yang <i@keitheis.org>
2013-06-10 13:46:31 +0800

    Using pprint.pformat and pygments for renderings value.

--- pyramid_debugtoolbar/panels/renderings.py
+++ pyramid_debugtoolbar/panels/renderings.py
@@ -1,7 +1,26 @@
+from pprint import pformat
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
+
 from pyramid_debugtoolbar.panels import DebugPanel
 from pyramid_debugtoolbar.utils import dictrepr
 
 _ = lambda x: x
+
+
+#class CodeHtmlFormatter(HtmlFormatter):
+    #def wrap(self, source, outfile):
+        #return self._wrap_code(source)
+
+    #def _wrap_code(self, source):
+        #for i, t in source:
+            #if t.startswith("u'"):
+                #t2 = t[2:-1]
+                #t = t2.decode('unicode-escape')
+            #yield i, t
+
 
 class RenderingsDebugPanel(DebugPanel):
     """
@@ -20,11 +39,17 @@ class RenderingsDebugPanel(DebugPanel):
             return
         val = getattr(event, 'rendering_val', '<unknown>')
         try:
-            val = repr(val)
+            val = pformat(val, width=160)
+            lexer = get_lexer_by_name("python")
+            formatter = HtmlFormatter(linenos=False, cssclass="source")
+            #formatter = CodeHtmlFormatter(linenos=False, cssclass="source")
+            val = highlight(val, lexer, formatter)
         except:
             # crazyass code raises an exception during __repr__ (formish)
             val = '<unknown>'
-        self.renderings.append(dict(name=name, system=dictrepr(event), val=val))
+        self.renderings.append(dict(name=name,
+                                    system=dictrepr(event),
+                                    val=val))
 
     def nav_title(self):
         return _('Renderings')
@@ -43,6 +68,4 @@ class RenderingsDebugPanel(DebugPanel):
         return self.render(
             'pyramid_debugtoolbar.panels:templates/renderings.dbtmako', {
             'renderings': self.renderings
-        }, request=self.request)
-
-
+            }, request=self.request)
